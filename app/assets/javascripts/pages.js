8b425a7236c036dc0232173eb2a6bbb7bdf2951a
Peter Jang <peter.x.jang@gmail.com>
2016-04-13 16:42:48 -0500

    fetch example with DOM

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,15 @@
-fetch('https://data.cityofchicago.org/resource/xzkq-xp2w.json')
-  .then(function(response) {
-    return response.json();
-  }).then(function(body) {
-    console.log(body);
-  });
+function getData() {
+  var divEmployees = document.getElementById("employees");
+
+  fetch('https://data.cityofchicago.org/resource/xzkq-xp2w.json')
+    .then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      var htmlString = '';
+      for (var i = 0; i < data.length; i++) {
+        var employee = data[i];
+        htmlString += '<h2>' + employee.name + '</h2>';
+      }
+      divEmployees.innerHTML = htmlString;
+    });
+}
