f95ea74b2112816aff6a24627c230582160717af
Peter Jang <peter.x.jang@gmail.com>
2016-04-13 17:51:36 -0500

    buttons for show police only/show all

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -9,7 +9,7 @@ function getData() {
       var htmlString = '';
       for (var i = 0; i < data.length; i++) {
         var employee = data[i];
-        htmlString += '<div onclick="changeEmployee(this)">';
+        htmlString += '<div class="employee" onclick="changeEmployee(this)">';
         htmlString += '<h2>' + employee.name + '</h2>';
         htmlString += '<p>Title: ' + employee.job_titles + '</p>';
         htmlString += '<p>Department: ' + employee.department + '</p>';
@@ -25,5 +25,25 @@ function changeEmployee(divEmployee) {
     divEmployee.style.color = '';
   } else {
     divEmployee.style.color = 'lightgray';
+  }
+}
+
+function showAll() {
+  var employees = document.querySelectorAll('.employee');
+  for (var i = 0; i < employees.length; i++) {
+    var employee = employees[i];
+    employee.style.display = '';
+  }
+}
+
+function showPolice() {
+  var employees = document.querySelectorAll('.employee');
+  for (var i = 0; i < employees.length; i++) {
+    var employee = employees[i];
+    if (employee.innerHTML.indexOf("POLICE") !== -1) {
+      employee.style.display = '';
+    } else {
+      employee.style.display = 'none';
+    }
   }
 }
