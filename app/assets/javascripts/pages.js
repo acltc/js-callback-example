e90f829e779d4e2d321266740f66413283fafbf7
Peter Jang <peter.x.jang@gmail.com>
2016-04-13 16:21:58 -0500

    working asynchronous example with DOM

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,8 @@
 function changeColors() {
   setTimeout(function() {
     document.getElementById('first').style.color = "red";
+    setTimeout(function() {
+      document.getElementById('second').style.color = "blue";
+    }, 1000);
   }, 1000);
-  document.getElementById('second').style.color = "blue";
 }
